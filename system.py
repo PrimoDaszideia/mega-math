ab66bfffcd42cef92a57e09ed283c5a1b922adbb
PrimoDaszideia <lucasintelite.suporte@gmail.com>
2025-04-24 15:59:28 -0300

    Alteracao 1

--- system.py
+++ system.py
@@ -1,2 +1,4 @@
-teste = 2
-jsjs = 1
+line = "================="
+
+print (line, 'SEJA BEM-VINDO AO MEGA MATH', line)
+service = int(input('Qual tipo dos nossos serviços deseja usar (Digite o numero referente o serviço): \n1- Tabuada\n2- Conversao de Temperatura\n3- Área de Figuras Geometricas\n4- Juros Simples\n5- Juros Compostos\n6- Calculadora Basica\n'))
